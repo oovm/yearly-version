42f9a809b1261651e15e4048de9379609d0f2a34
Aster <aster@vers.site>
2023-01-24 13:34:03 +0000

    🐣 Define the `YearlyVersion` structure

---
+++ projects/yearly-version/src/lib.rs
@@ -0,0 +1,17 @@
+#![deny(missing_debug_implementations, missing_copy_implementations)]
+#![warn(missing_docs, rustdoc::missing_crate_level_docs)]
+#![doc = include_str!("../readme.md")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+
+mod errors;
+
+pub use crate::errors::{Error, Result};
+
+pub struct YearlyVersion {
+    pub year: u32,
+    pub major: u16,
+    pub minor: u16,
+    pub patch: u16,
+    pub tag: String,
+}
